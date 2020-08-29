64d41e90fe8c4d0a403c9c5f87ae2eb2f446e968
david haioum <dav6983@gmail.com>
2020-08-29 11:10:52 +0200

    feat: about modal style done add: boolean value to disable splashscreen in config

--- src/main/kotlin/com/david/mocassin/view/components/fragments/AboutModal.kt
+++ src/main/kotlin/com/david/mocassin/view/components/fragments/AboutModal.kt
@@ -1,7 +1,10 @@
 package com.david.mocassin.view.components.fragments
 
+import javafx.geometry.Pos
+import javafx.scene.control.SelectionMode
 import javafx.scene.image.ImageView
 import tornadofx.*
+import java.awt.Color
 import java.time.LocalDate
 
 class AboutModal: Fragment("") {
@@ -12,23 +15,94 @@ class AboutModal: Fragment("") {
     }
 
     override val root = vbox {
-        label(messages["created_by"])
+        hyperlink(messages["created_by"]) {
+            paddingBottom = 12
 
-        label("Copyright ©${LocalDate.now().year}")
-
-        label("logo designed by @saiko2b")
-
-        hbox {
-            button("Github").action {
-                hostServices.showDocument("https://github.com/david6983/mocassin")
+            setOnAction {
+                hostServices.showDocument("https://github.com/david6983")
             }
 
-            button("Logo designer").action {
+            border = null
+
+            textFill = c("black")
+
+            tooltip("https://github.com/david6983")
+        }
+
+        hyperlink("logo designed by @saiko2b") {
+            paddingBottom = 12
+
+            setOnAction {
                 hostServices.showDocument("https://www.instagram.com/saiko2b/")
+            }
+
+            border = null
+
+            textFill = c("black")
+
+            tooltip("https://www.instagram.com/saiko2b/")
+        }
+
+        label("Copyright ©${LocalDate.now().year}") {
+            paddingBottom = 12
+            style {
+                fontSize = 14.0.px
             }
         }
 
+        label("Useful links: ") {
+            paddingBottom = 12
+            style {
+                fontSize = 16.0.px
+            }
+            textFill = c("black")
+        }
+
+
+        listview<String> {
+            items.add("Mocassin official repository")
+            items.add("Logo designer instagram")
+            items.add("Developer github repository")
+
+            selectionModel.selectionMode = SelectionMode.SINGLE
+
+            onDoubleClick {
+                when(selectionModel.selectedIndex) {
+                    1 -> hostServices.showDocument("https://github.com/david6983/mocassin")
+                    2 -> hostServices.showDocument("https://www.instagram.com/saiko2b/")
+                    3 -> hostServices.showDocument("https://github.com/david6983/")
+                }
+            }
+        }
+
+        label("*click on the authors name to see more") {
+            textFill = c("red")
+            paddingTop = 12
+            style {
+                fontSize = 12.0.px
+            }
+        }
+
+        label("**double click on a link in the list above") {
+            textFill = c("green")
+            paddingBottom = 5
+            style {
+                fontSize = 12.0.px
+            }
+        }
+
+        style {
+            fontSize = 16.0.px
+        }
+
+        vboxConstraints {
+            paddingTop = 12.0
+            paddingBottom = 12.0
+            paddingLeft = 12.0
+            paddingRight = 12.0
+        }
+
         prefWidth = 250.0
-        prefHeight = 140.0
+        prefHeight = 286.0
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,7 @@ import com.david.mocassin.utils.lang.SupportedLocale
 import com.david.mocassin.view.MainView
 import com.david.mocassin.view.components.fragments.SplashScreen
 import com.david.mocassin.view.styles.MainStyle
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.scene.image.Image
 import javafx.stage.Stage
 import javafx.stage.StageStyle
@@ -10,6 +11,7 @@ import java.util.*
 
 class MocassinApp: App(MainView::class, MainStyle::class) {
     //override val configBasePath = Paths.get("/etc/mocassin/conf")
+    val isSplashScreenDisplayed = SimpleBooleanProperty(true)
 
     init {
         addStageIcon(Image(resources["icons/mocassin.png"]))
@@ -22,9 +24,20 @@ class MocassinApp: App(MainView::class, MainStyle::class) {
             FX.locale = loc.local
         }
 
+        config["splashscreen"]?.let {
+            isSplashScreenDisplayed.value = it as Boolean?
+        }
+
         FX.localeProperty().onChange {
             with(config) {
                 set("locale" to it)
+                save()
+            }
+        }
+
+        isSplashScreenDisplayed.onChange {
+            with(config) {
+                set("splashscreen" to it)
                 save()
             }
         }
@@ -32,10 +45,12 @@ class MocassinApp: App(MainView::class, MainStyle::class) {
 
     override fun start(stage: Stage) {
         super.start(stage)
-        find<SplashScreen>().openModal(stageStyle = StageStyle.TRANSPARENT)
+        if (isSplashScreenDisplayed.value) {
+            find<SplashScreen>().openModal(stageStyle = StageStyle.TRANSPARENT)
+        }
     }
 
-    override fun shouldShowPrimaryStage() = false
+    override fun shouldShowPrimaryStage() = !isSplashScreenDisplayed.value
 }
 
 fun main(args: Array<String>) {
